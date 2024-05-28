a0a3e1045d0a40396ef71fdd1b0fd76b9f9d9601
vigneshravi2396 <142408589+vigneshravi2396@users.noreply.github.com>
2024-05-28 18:18:47 +0530

    Delete VPC/output.tf

--- VPC/output.tf
+++
@@ -1,9 +0,0 @@
-output "db_creds" {
-  value       = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
-  description = "Decoded database credentials obtained from AWS Secrets Manager"
-}
-
-output "SECRETARN" {
-  value       = aws_secretsmanager_secret.secret_password.arn
-  description = "ARN of the AWS Secrets Manager secret"
-}
